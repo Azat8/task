7cf2d36726e20aabbfcc976266d3bcd8500b6cf5
Mkhitaryan <vahan.mkhitaryan.1999@mail.ru>
2019-05-06 16:03:08 +0400

    06.05.19

---
+++ task/config/.env.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'mysql' => [
+        'user' => 'root',
+        'database' => 'task',
+        'password' => '',
+        'server' => 'localhost'
+    ],
+
+    'maria_db' => [
+        'user' => '',
+        'database' => '',
+        'password' => '',
+        'server' => ''
+    ],
+
+    'pdo' => [
+        'user' => '',
+        'database' => '',
+        'password' => '',
+        'server' => ''
+
+    ]
+];
